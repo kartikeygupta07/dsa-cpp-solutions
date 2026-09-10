4cd049b271e9dedefd514c4c24627cee90ba4746
Geeky Mind <kartikeygupta185@gmail.com>
2026-09-10 23:52:30 +0530

    Time: 0 ms (100%), Space: 22.3 MB (27.57%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -16,9 +16,9 @@ public:
             return 0;
         }
 
-        int lh = maxDepth(root -> left);
-        int rh = maxDepth(root -> right);
+        int leftHeight = maxDepth(root -> left);
+        int rightHeight = maxDepth(root -> right);
 
-        return 1 + max(lh, rh); 
+        return 1 + max(leftHeight , rightHeight);
     }
 };
